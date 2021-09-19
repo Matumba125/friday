c8c51c856c09dbed011cabc9b8567026706eee27
AlexandrShnipov <sshnipov@gmail.com>
2021-09-19 22:58:32 +0300

    added passwordControl

--- src/components/loginization/Loginization.tsx
+++ src/components/loginization/Loginization.tsx
@@ -15,14 +15,19 @@ const Loginization = () => {
                         {/* <span className={s.errorText}>This field is requared</span> */}
                     </label>
 
-                    <label  className={s.formItem}>
+                    <label className={s.formItem}>
                         <p className={s.inputText}> Password</p>
-                        <input className={s.input} type="password" placeholder="Please enter password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*" title="Password must contain at least 8 characters, one number, one uppercase letter and one lowercase" required />
+                        <div className={s.passwordWrap}>
+                            <input className={s.input} type="password" placeholder="Please enter password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*" title="Password must contain at least 8 characters, one number, one uppercase letter and one lowercase" required />
+
+                            <button className={s.passwordControl}>
+                            </button>
+                        </div>
                     </label>
 
-                   <div className={s.linkWrap}>
+                    <div className={s.linkWrap}>
                         <Link className={s.passForgot} to={'/password-recovery'}>Forgot password</Link>
-                   </div>
+                    </div>
 
                     <div className={s.buttonWrap}>
                         <button className={s.submitButton} type="submit">Login</button>
