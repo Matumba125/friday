a87c944f71c87fbe7e0fa451302a4163e53615b5
AlexandrShnipov <sshnipov@gmail.com>
2021-09-20 17:46:01 +0300

    added component - buttonFormColor

---
+++ src/components/buttonFormColor/ButtonFormColor.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from './ButtonFormColor.module.css'
+
+type ButtonFormColorPropsType = {
+    text: string
+}
+
+
+const ButtonFormColor = (props: ButtonFormColorPropsType) => {
+
+    return (
+       
+            <button className={s.submitButton} type="submit">{props.text}</button>
+    )
+}
+
+export default ButtonFormColor
--- src/components/loginization/Loginization.tsx
+++ src/components/loginization/Loginization.tsx
@@ -4,6 +4,7 @@ import s from './Loginization.module.css';
 import GlobalTitle from '../globalTitle/GlobalTitle';
 import ListTitle from '../listTitle/ListTitle';
 import InputForm from '../inputForm/InputForm';
+import ButtonFormColor from '../buttonFormColor/ButtonFormColor';
 
 const Loginization = () => {
 
@@ -12,7 +13,6 @@ const Loginization = () => {
     const onViewPass = () => {
         setViewPass(state => !state)
     }
-   
     // !password control
 
     return (
@@ -41,40 +41,16 @@ const Loginization = () => {
                         onViewPass={onViewPass}
                     />
 
-                    {/* <label className={s.formItem}>
-                        <p className={s.inputText}> Email</p>
-                        <div className={s.inputBox}>
-                            <input className={s.input}
-                                type="email"
-                                placeholder="Please enter email"
-                                pattern=""
-                                title="Please enter email"
-                                required />
-                        </div>
-
-                    </label> */}
-
-                    {/* <label className={s.formItem}>
-                        <p className={s.inputText}> Password</p>
-                        <div className={s.inputBox}>
-                            <input className={s.input}
-                                type={viewPass ? "text" : "password"}
-                                placeholder="Please enter password"
-                                pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*"
-                                title="Password must contain at least 8 characters, one number, one uppercase letter and one lowercase"
-                                required />
-
-                            <button className={viewPassStyle} onClick={onViewPass} />
-                        </div>
-                    </label> */}
 
                     <div className={s.linkWrap}>
                         <Link className={s.passForgot} to={'/password-recovery'}>Forgot password</Link>
                     </div>
 
-                    <div className={s.buttonWrap}>
-                        <button className={s.submitButton} type="submit">Login</button>
+                    <div className={s.buttonContainer}>
+                        <ButtonFormColor
+                            text='Login' />
                     </div>
+
 
                     <p className={s.formText}>Don’t have an account?</p>
 
