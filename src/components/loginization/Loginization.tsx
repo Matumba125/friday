030ad43772f3b75ad558cb3c313441948bd85a8c
matumba 125 <nbenza5@gmail.com>
2021-09-22 14:40:41 +0300

    added loginization

--- src/components/loginization/Loginization.tsx
+++ src/components/loginization/Loginization.tsx
@@ -1,35 +1,62 @@
-import React from 'react';
-import './Loginization.module.css';
+
 import { Link } from 'react-router-dom';
+import s from './Loginization.module.css';
+import CardContainer from '../cardContainer/CardContainer';
+import GlobalTitle from '../globalTitle/GlobalTitle';
+import ListTitle from '../listTitle/ListTitle';
+import InputForm from '../inputForm/InputForm';
+import ButtonFormColor from '../buttonFormColor/ButtonFormColor';
+import { PATH } from '../routing/Routing';
 
 const Loginization = () => {
+
     return (
-        <div className="container">
-            <div className="containerInner">
-                <h1 className="globalTitle">It-incubator</h1>
-                <h2 className="listTitle">Sign In</h2>
-                <form action="" method="">
-                    <label>
-                        <p className="inputText"> Email</p>
-                        <input type="email" placeholder="Please enter email" required/>
-                        <span className="errorText">This field is requared</span>
-                    </label>
 
-                    <label>
-                        <p className="inputText"> Password</p>
-                        <input type="password" placeholder="Please enter password" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*" title="password must contain at least 8 characters, one number, one uppercase letter and one lowercase" required/>
-                    </label>
+        <CardContainer>
+            <>
+                <div className={s.globalTitleBox}>
+                    <GlobalTitle />
+                </div>
 
-                    <Link className="passForgot" to={'/password-recovery'}>Forgot password</Link>
 
-                    <button className="submitButton" type="submit">Login</button>
+                <div className={s.listTitleBox}>
+                    <ListTitle />
+                </div>
 
-                    <p className="formText">Don’t have an account?</p>
+                <form className={s.formWrap} action="" method="">
 
-                    <Link className="linkRegistration" to={'/registration'}>Sign Up</Link>
+                    <InputForm
+                        text={'Email'}
+                        type={'email'}
+                        placeholder={'Please enter email'}
+                        title={'Please enter email'}
+                    />
+
+                    <InputForm
+                        text={'Password'}
+                        type={'password'}
+                        placeholder={'Please enter password'}
+                        pattern={'(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'}
+                        title={'the password must be at least 6 characters long including, one number, one capital letter, one small letter, and one of the special characters ! @ # $% ^ & *'}
+                    />
+
+
+                    <div className={s.linkWrap}>
+                        <Link className={s.passForgot} to={'/password-recovery'}>Forgot password</Link>
+                    </div>
+
+                    <div className={s.buttonContainer}>
+                        <ButtonFormColor
+                            text='Login' />
+                    </div>
+
+
+                    <p className={s.formText}>Don’t have an account?</p>
+
+                    <Link className={s.linkCardBottom} to={PATH.REGISTRATION}>Sign Up</Link>
                 </form>
-            </div>
-        </div>
+            </>
+        </CardContainer>
     );
 };
 
