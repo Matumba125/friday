1ee8ac3e9dd547ad5f585f4d3968bd1960b3d277
AlexandrShnipov <sshnipov@gmail.com>
2021-09-24 20:21:45 +0300

    added IsLoading

--- src/components/registration/Registration.tsx
+++ src/components/registration/Registration.tsx
@@ -6,10 +6,11 @@ import ListTitle from '../../common/listTitle/ListTitle';
 import InputForm from '../../common/inputForm/InputForm';
 import ButtonFormColor from '../../common/buttonFormColor/ButtonFormColor';
 import ButtonFormCancel from '../../common/buttonFormCancel/ButtonFormCancel'
-import {Redirect, useHistory } from 'react-router';
+import IsLoading from '../../common/isLoading/IsLoading';
+import { Redirect, useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { registerTC, setErrorAC } from '../../store/registrationReducer';
-import {getIsLoading, getRegistrationError, getRegistrationStatus } from '../../store/selectots';
+import { getIsLoading, getRegistrationError, getRegistrationStatus } from '../../store/selectots';
 import { PATH } from '../routing/Routing';
 
 const Registration = () => {
@@ -25,103 +26,111 @@ const Registration = () => {
     const [confirmPassword, setConfirmPassword] = useState<string>('')
     const [passwordsUnmatch, setPasswordsUnmatch] = useState<boolean>(false)
 
-    const onEmailChangeHandler = (gainedEmail: string) =>{
+    const onEmailChangeHandler = (gainedEmail: string) => {
         setEmail(gainedEmail)
-        if(error) dispatch(setErrorAC(''))
+        if (error) dispatch(setErrorAC(''))
     }
-    const onPasswordChangeHandler = (gainedPassword: string) =>{
+    const onPasswordChangeHandler = (gainedPassword: string) => {
         setPassword(gainedPassword)
     }
-    const onPasswordConfirmChangeHandler = (gainedPasswordConfirm: string) =>{
+    const onPasswordConfirmChangeHandler = (gainedPasswordConfirm: string) => {
         setConfirmPassword(gainedPasswordConfirm)
         setPasswordsUnmatch(false)
     }
 
-    const onClickHandler = () =>{
-        if(password === confirmPassword){ dispatch(registerTC(
-            {
-                email: email,
-                password: password
-            }
-        ))} else {
+    const onClickHandler = () => {
+        if (password === confirmPassword) {
+            dispatch(registerTC(
+                {
+                    email: email,
+                    password: password
+                }
+            ))
+        } else {
             setPasswordsUnmatch(true)
         }
 
     }
 
-    if(registered){
-        return <Redirect to={PATH.LOGIN}/>
+    if (registered) {
+        return <Redirect to={PATH.LOGIN} />
     }
 
     return (
+        <>
 
-        <CardContainer>
-            <>
-                <div className={s.globalTitleBox}>
-                    <GlobalTitle />
-                </div>
+{/*  modale - circleadd */}
+            {/* <IsLoading /> */}
 
-                <div className={s.listTitleBox}>
-                    <ListTitle
-                    text='Sign Up'
-                    />
-                </div>
+            <CardContainer>
+                <>
 
-                <form className={s.formWrap} action="" method="">
-
-                    <InputForm
-                        text={'Email'}
-                        inputType={'email'}
-                        placeholder={'Please enter email'}
-                        title={'Please enter email'}
-                        value={email}
-                        onChangeText={onEmailChangeHandler}
-                    />
-
-                    <InputForm
-                        text={'Password'}
-                        inputType={'password'}
-                        placeholder={'Please enter password'}
-                        pattern={'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'}
-                        title={'the password must be at least 6 characters long including, one number, one capital letter, one small letter, and one of the special characters ! @ # $% ^ & *'}
-                        value={password}
-                        onChangeText={onPasswordChangeHandler}
-                    />
-
-                    <InputForm
-                        text={'Сonfirm password'}
-                        inputType={'password'}
-                        placeholder={'Please confirm password'}
-                        pattern={'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'}
-                        title={'Please confirm password'}
-                        value={confirmPassword}
-                        onChangeText={onPasswordConfirmChangeHandler}
-                    />
-
-                    {
-                        error && <p className={s.error}>{error}</p>
-                    }
-
-                    {
-                        passwordsUnmatch && <p className={s.error}>Passwords don't match</p>
-                    }
-
-                    <div className={s.buttonsContainer}>
-
-                        <ButtonFormCancel disabled={isLoading}/>
-
-                        <div className={s.buttonContainer}>
-                            <ButtonFormColor
-                                text='Register'
-                                callback={onClickHandler}
-                                disabled={isLoading}
-                            />
-                        </div>
+                    <div className={s.globalTitleBox}>
+                        <GlobalTitle />
                     </div>
 
-                </form>
-            </>
-        </CardContainer>
+                    <div className={s.listTitleBox}>
+                        <ListTitle
+                            text='Sign Up'
+                        />
+                    </div>
+
+                    <form className={s.formWrap} action="" method="">
+
+                        <InputForm
+                            text={'Email'}
+                            inputType={'email'}
+                            placeholder={'Please enter email'}
+                            title={'Please enter email'}
+                            value={email}
+                            onChangeText={onEmailChangeHandler}
+                        />
+
+                        <InputForm
+                            text={'Password'}
+                            inputType={'password'}
+                            placeholder={'Please enter password'}
+                            pattern={'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'}
+                            title={'the password must be at least 6 characters long including, one number, one capital letter, one small letter, and one of the special characters ! @ # $% ^ & *'}
+                            value={password}
+                            onChangeText={onPasswordChangeHandler}
+                        />
+
+                        <InputForm
+                            text={'Сonfirm password'}
+                            inputType={'password'}
+                            placeholder={'Please confirm password'}
+                            pattern={'(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'}
+                            title={'Please confirm password'}
+                            value={confirmPassword}
+                            onChangeText={onPasswordConfirmChangeHandler}
+                        />
+
+                        {
+                            error && <p className={s.error}>{error}</p>
+                        }
+
+                        {
+                            passwordsUnmatch && <p className={s.error}>Passwords don't match</p>
+                        }
+
+                        <div className={s.buttonsContainer}>
+
+                            <ButtonFormCancel disabled={isLoading} />
+
+                            <div className={s.buttonContainer}>
+                                <ButtonFormColor
+                                    text='Register'
+                                    callback={onClickHandler}
+                                    disabled={isLoading}
+                                />
+                            </div>
+                        </div>
+
+                    </form>
+                </>
+            </CardContainer>
+        </>
     );
 };
 
