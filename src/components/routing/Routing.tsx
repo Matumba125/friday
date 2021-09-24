30306469abd6afe64cd1736354ba711ba7708b72
matumba 125 <nbenza5@gmail.com>
2021-09-24 17:49:02 +0300

    fixed routings for gh-pages

--- src/components/routing/Routing.tsx
+++ src/components/routing/Routing.tsx
@@ -8,18 +8,18 @@ import Profile from "../profile/Profile";
 import Registration from "../registration/Registration";
 
 export const PATH ={
-    LOGIN: '/login',
-    NOT_FOUND: '/404',
-    NEW_PASSWORD: '/new-password',
-    PASSWORD_RECOVERY: '/password-recovery',
-    PROFILE: '/profile',
-    REGISTRATION: '/registration',
+    LOGIN: 'friday/login',
+    NOT_FOUND: 'friday/404',
+    NEW_PASSWORD: 'friday/new-password',
+    PASSWORD_RECOVERY: 'friday/password-recovery',
+    PROFILE: 'friday/profile',
+    REGISTRATION: 'friday/registration',
 }
 const Routing = () => {
     return (
         <>
             <Switch>
-                <Route exact path={'/'} render={()=> <Redirect to={PATH.LOGIN}/>} />
+                <Route exact path={'friday/'} render={()=> <Redirect to={PATH.LOGIN}/>} />
                 <Route path={PATH.LOGIN} render={()=> <Loginization/>} />
                 <Route path={PATH.NOT_FOUND} render={()=> <NotFound/>} />
                 <Route path={PATH.NEW_PASSWORD} render={()=> <NewPassword/>} />
