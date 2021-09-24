aba9078c952496269fb46d885687a6b4b23a5ca4
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-09-24 12:48:09 +0300

    add setIsLoggedInAC

--- src/components/loginization/Loginization.tsx
+++ src/components/loginization/Loginization.tsx
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom';
 import s from './Loginization.module.css';
 import CardContainer from '../../common/cardContainer/CardContainer';
@@ -7,58 +6,70 @@ import ListTitle from '../../common/listTitle/ListTitle';
 import InputForm from '../../common/inputForm/InputForm';
 import ButtonFormColor from '../../common/buttonFormColor/ButtonFormColor';
 import { PATH } from '../routing/Routing';
+import { useState } from 'react';
 
 const Loginization = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const onEmailChangeHandler = (email: string) => {
+        setEmail(email);
+    };
+    const onPasswordChangeHandler = (email: string) => {
+        setPassword(password);
+    };
 
     return (
-
         <CardContainer>
             <>
-            <div className={s.globalTitleBox}>
-                <GlobalTitle />
-            </div>
-
+                <div className={s.globalTitleBox}>
+                    <GlobalTitle />
+                </div>
 
                 <div className={s.listTitleBox}>
-                    <ListTitle
-                        text='Sign In'
-                    />
+                    <ListTitle text="Sign In" />
                 </div>
 
-            <form className={s.formWrap} action="" method="">
+                <form className={s.formWrap} action="" method="">
+                    <InputForm
+                        text={'Email'}
+                        inputType={'email'}
+                        placeholder={'Please enter email'}
+                        title={'Please enter email'}
+                        value={email}
+                        onChangeText={onEmailChangeHandler}
+                    />
 
-                <InputForm
-                    text={'Email'}
-                    inputType={'email'}
-                    placeholder={'Please enter email'}
-                    title={'Please enter email'}
-                />
-
-                <InputForm
-                    text={'Password'}
-                    inputType={'password'}
-                    placeholder={'Please enter password'}
-                    pattern={'(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'}
-                    title={'the password must be at least 6 characters long including, one number, one capital letter, one small letter, and one of the special characters ! @ # $% ^ & *'}
-                />
-
+                    <InputForm
+                        text={'Password'}
+                        inputType={'password'}
+                        placeholder={'Please enter password'}
+                        pattern={
+                            '(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}'
+                        }
+                        title={
+                            'the password must be at least 6 characters long including, one number, one capital letter, one small letter, and one of the special characters ! @ # $% ^ & *'
+                        }
+                        value={password}
+                        onChangeText={onPasswordChangeHandler}
+                    />
 
                     <div className={s.linkWrap}>
-                        <Link className={s.passForgot} to={PATH.PASSWORD_RECOVERY}>Forgot password</Link>
+                        <Link className={s.passForgot} to={PATH.PASSWORD_RECOVERY}>
+                            Forgot password
+                        </Link>
                     </div>
 
-                <div className={s.buttonContainer}>
-                    <ButtonFormColor
-                        text='Login' />
-                </div>
+                    <div className={s.buttonContainer}>
+                        <ButtonFormColor text="Login" />
+                    </div>
 
-
-                <p className={s.formText}>Don’t have an account?</p>
-
+                    <p className={s.formText}>Don’t have an account?</p>
                 </form>
 
-                <Link className={s.linkCardBottom} to={PATH.REGISTRATION}>Sign Up</Link>
-
+                <Link className={s.linkCardBottom} to={PATH.REGISTRATION}>
+                    Sign Up
+                </Link>
             </>
         </CardContainer>
     );
--- src/store/loginizationReducer.ts
+++ src/store/loginizationReducer.ts
@@ -1,5 +1,16 @@
 
 
-export const loginizationReducer =(state: any, action: any)=>{
+const inititialState = { isLoggetIn: false }
+type LoginizetionReducerInititialStateType = typeof inititialState
+
+type ActionType = ReturnType<typeof setIsLoggedInAC>
+
+export const loginizationReducer = (state: LoginizetionReducerInititialStateType = inititialState, action: ActionType): LoginizetionReducerInititialStateType => {
+    switch (action.type) {
+        case 'login/SET-IS-LOGGED-IN':
+            return { ...state, isLoggetIn: action.value }
+    }
     return state
 }
+
+export const setIsLoggedInAC = (value: boolean) => ({ type: 'login/SET-IS-LOGGED-IN', value } as const)
