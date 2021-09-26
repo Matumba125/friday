b78d8ec26fa9d663afb35d71361e6687d937d21a
matumba 125 <nbenza5@gmail.com>
2021-09-26 11:51:36 +0300

    added thunk for updating profile

--- src/store/profileReducer.ts
+++ src/store/profileReducer.ts
@@ -1,6 +1,45 @@
+import { UserDataType } from "../api/auth-api"
 
-const initialState = {}
-
-export const profileReducer = (state: any = initialState, action: any) =>{
-    return state
+type SetUserDataType ={
+    type: 'PROFILE/SET-USER-DATA',
+    data: UserDataType
 }
+
+type ProfileReducerActionType = SetUserDataType
+
+const initialState = {
+    userData: {
+        _id: '',
+        email: '',
+        name: '',
+        avatar: '',
+        publicCardPacksCount: 0,
+
+        created: new Date(),
+        updated: new Date(),
+        isAdmin: false,
+        verified: false,
+        rememberMe: false,
+
+
+    }
+}
+
+type ProfileReducerInitialStateType = {
+    userData: UserDataType
+}
+
+export const profileReducer = (state:ProfileReducerInitialStateType = initialState, action: ProfileReducerActionType): ProfileReducerInitialStateType =>{
+    switch (action.type) {
+        case 'PROFILE/SET-USER-DATA':
+            return {
+                ...state,
+                userData: action.data
+            }
+        default:
+            return state
+    }
+
+}
+
+export const setUserDataAC = (data: UserDataType): SetUserDataType => ({type: 'PROFILE/SET-USER-DATA', data})
