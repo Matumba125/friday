3a72f1b72978f127c318319c81d525d13ccc7df4
matumba 125 <nbenza5@gmail.com>
2021-10-08 18:35:08 +0300

    fixed auth bugs

--- src/store/authReducer.ts
+++ src/store/authReducer.ts
@@ -2,13 +2,14 @@ import {createAsyncThunk} from "@reduxjs/toolkit"
 import {authApi} from "../api/auth-api"
 import {setIsLoading} from "./appReducer"
 import {setLoggedAC} from "./loginizationReducer"
+import { setUserDataAC } from "./profileReducer"
 
 export const authMeTC = createAsyncThunk('auth/authMe', async (param, {dispatch, rejectWithValue}) => {
     try {
         dispatch(setIsLoading({isLoading: true}))
         const res = await authApi.me()
         dispatch(setLoggedAC({isLoggedIn: true}))
-        return {userData: res.data}
+        dispatch(setUserDataAC({userData: res.data}))
     } catch (error) {
         return rejectWithValue(error)
     } finally {
